74b42fbc5ccd04f85af54790eed4a45816b0c7f8
Rahul4dev <rahulsinghsignin@gmail.com>
2022-12-28 22:47:47 +0530

    added some base styles and first form

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const path = require("path"); // builtin path module for file directory to look 
 const express = require("express");
 
 const authRoutes = require("./routes/auth.routes");
+const exp = require("constants");
 
 const app = express();
 
 app.set("view engine", "ejs"); // to view our pages
 app.set("views", path.join(__dirname, "views")); // location of our views pages
+
+app.use(express.static('public')); // it will provide this folder to all the files statically
 
 app.use(authRoutes); // check for every incoming request
 
