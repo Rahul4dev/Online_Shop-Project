b564cef3e00d9d6b4aed66644bbfc9c24e61d1a1
Rahul4dev <rahulsinghsignin@gmail.com>
2022-12-31 20:50:39 +0530

    added session and error handler

--- app.js
+++ app.js
@@ -2,11 +2,15 @@
 const path = require("path"); // builtin path module for file directory to look for the files related to the app.js
 
 const express = require("express");
+const csurf = require('csurf');
+const expressSession = require('express-session');
 
+const createSessionConfig = require('./config/session');
 const db = require('./data/database');
-
+const addCsrfTokenMiddleware = require('./middleware/csrf-token');
 const authRoutes = require("./routes/auth.routes");
 
+const errorHandlerMiddleware = require('./middleware/errorHandler');
 
 const app = express();
 
@@ -16,16 +20,26 @@ app.set("views", path.join(__dirname, "views")); // location of our views pages
 app.use(express.static('public')); // it will provide this folder to all the files statically
 app.use(express.urlencoded({ extended: false}));
 
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig));
+app.use(csurf());
+
+
+app.use(addCsrfTokenMiddleware);
+
 app.use(authRoutes); // check for every incoming request
+
+app.use(errorHandlerMiddleware);
 
 db.connectToDatabase()
     .then(function() {
-        app.listen(3000);
+        app.listen(3000);  // <our domain which we listen>
     })
     .catch(function(error) {
         console.log('Failed to connect to database');
         console.log(error);
     });
- // <our domain which we listen>
+ 
 
 // </Rahul>
---
+++ config/session.js
@@ -0,0 +1,32 @@
+const mongoDbStore = require("connect-mongodb-session");
+const expressSession =  require('express-session');
+
+function createSessionStore() {
+    const MongoDbStore = mongoDbStore(expressSession);
+
+    const store = new MongoDbStore({
+      uri: "mongodb://127.0.0.1:27017",
+      databaseName: "online-shop",
+      collection: "sessions",
+    });
+
+    store.on('error', (error) => {
+        console.log(error);
+    })
+
+     return store;
+}
+
+function createSessionConfig() {
+    return {
+        secret: 'super-secret',  // we should select it long and complex string
+        resave: false,
+        saveUninitialized: false,
+        store: createSessionStore(),
+        cookie: {
+            maxAge: 2 * 24* 60*60*1000
+        }
+    };
+} 
+
+module.exports = createSessionConfig;
