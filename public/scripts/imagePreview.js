136e1478bd5928845c295203e8a2c648864661be
Rahul4dev <rahulsinghsignin@gmail.com>
2023-01-10 11:44:03 +0530

    Added image Preview feature in update product

---
+++ public/scripts/imagePreview.js
@@ -0,0 +1,18 @@
+const imagePickerEle = document.querySelector('#image-upload-control input');
+const imagePreviewElement = document.querySelector('#image-upload-control img');
+
+function updateImagePreview() {
+    const files = imagePickerEle.files;
+
+    if(!files  || files.length === 0) {
+        imagePreviewElement.style.display = 'none';
+        return;
+    }
+
+    const pickedFile = files[0];
+
+    imagePreviewElement.src = URL.createObjectURL(pickedFile);
+    imagePreviewElement.style.display = 'block';
+} 
+
+imagePickerEle.addEventListener('change', updateImagePreview);
